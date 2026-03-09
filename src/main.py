99bcc93bd8f981f66f9f657b17e812b9cad4ca46
anajorbaa <ana.jorba@alumnos.upm.es>
2026-03-09 18:46:40 +0100

    mas main

--- src/main.py
+++ src/main.py
@@ -47,13 +47,18 @@ result = solve_newton_case(
 # ============================================================
 # RESULTADOS BÁSICOS
 # ============================================================
+CF = result["CF_total"]
+CM = result["CM_total"]
+
 print("\n=== RESULTADOS NEWTON ===")
 print(f"alpha_deg = {result['alpha_deg']}")
-print(f"CF_total  = {result['CF_total']}")
-print(f"CM_total  = {result['CM_total']}")
 
-print("\nPrimeros 10 valores de cp:")
-print(result["cp"][:10])
+print(f"CF_total_x = {CF[0]}")
+print(f"CF_total_y = {CF[1]}")
+print(f"CF_total_z = {CF[2]}")
 
+print(f"CM_total_x = {CM[0]}")
+print(f"CM_total_y = {CM[1]}")
+print(f"CM_total_z = {CM[2]}")
 print("\nNúmero de caras a barlovento:", np.sum(result["mu"] > 0.0))
 print("Número de caras a sotavento :", np.sum(result["mu"] <= 0.0))
